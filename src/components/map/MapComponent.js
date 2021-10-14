b60d16ff890eec3c8a30667592ce220e5f30aa2a
Mikko Partanen <mikko.partanen93@gmail.com>
2021-10-14 23:58:33 +0300

    Päivitetään ChangeViewillä kartan keskitys, mikäli Junan sijainti on muuttunut kartalla

--- src/components/map/MapComponent.js
+++ src/components/map/MapComponent.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup, LayersControl } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, useMap, MapConsumer } from "react-leaflet";
 import { Component } from "react";
 //import markerIconPng from "leaflet/dist/images/marker-icon.png" //basic-marker
 import markerTrainIconPng from "./../../assets/images/train-tunnel.svg"; //train marker
@@ -9,9 +9,10 @@ import DigitrafficService from './../../services/DigitrafficService';
 const position = [62.24147, 25.72088];
 
 
-
+//Karttatasot omaan funktioon
 function MapLayersControl() {
-    return (<LayersControl position="topright">
+    return (
+    <LayersControl position="topright">
       <LayersControl.BaseLayer checked name="Mapnik">
         <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </LayersControl.BaseLayer>
@@ -27,26 +28,31 @@ function MapLayersControl() {
       <LayersControl.BaseLayer name="Dark all">
         <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" />
       </LayersControl.BaseLayer>
-      
       //Rautatie-taso kartalle
-      <LayersControl.Overlay name="Raide-taso">
+      <LayersControl.Overlay checked name="Raide-taso">
         <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png" />
       </LayersControl.Overlay>
     </LayersControl>);
 }
 
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  map.setView(center, map.getZoom()); //käytä zoomia jos haluat vakioida zoomin
+  return null;
+}
 
 class MapComponent extends Component { 
   state = {
     pollingCount: 0,
-    delay: 3000,
-    coordinates: [62.24147, 25.72088] 
+    delay: 4000,
+    coordinates: [62.24147, 25.72088],
+    zoom: 13
   }
   componentDidMount() {
     this.interval = setInterval(this.tick, this.state.delay)
   }
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.delay != this.state.delay) {
+    if(prevState.delay !== this.state.delay) {
       clearInterval(this.interval);
       this.interval = setInterval(this.tick, this.steta.delay);
     } 
@@ -55,13 +61,19 @@ class MapComponent extends Component {
     clearInterval(this.interval);
   }
 
+  changeCurrentZoom(value) {
+    this.setState({
+      zoom: value
+    })
+  }
+
   tick = () => {
+    //TODO tee dropdown josta voi hakea junan id:n
     DigitrafficService.getLatestCoordinate(73).then(data => {
-      console.log(data);
       let coordinate = [data[0].location.coordinates[1], data[0].location.coordinates[0]]
       this.setState({
         pollingCount: this.state.pollingCount + 1,
-        coordinates: coordinate
+        coordinates: coordinate,
       }); 
     }).catch(error => {
       console.log(error);
@@ -69,10 +81,24 @@ class MapComponent extends Component {
   }
 
   render() {
+    let currentZoom = this.state.zoom;
     return (
       <div id="mapid">
       <h2>PollingCount: {this.state.pollingCount}</h2>
-      <MapContainer center={this.state.coordinates} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={this.state.coordinates} zoom={this.state.zoom} scrollWheelZoom={true}>
+      {/* A JSX comment 
+      Tässä esimerkkinä miten voidaan käyttää myös mapconsumeria
+      <MapConsumer>
+        {(map) => {
+          if(currentZoom !== map.getZoom()) {
+            map.setView(map.getCenter(), map.getZoom());
+          }
+          return null
+        }}
+      </MapConsumer>
+      */}
+      <ChangeView center={this.state.coordinates} zoom={currentZoom} >
+      </ChangeView>
       <MapLayersControl></MapLayersControl>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- src/services/DigitrafficService.js
+++ src/services/DigitrafficService.js
@@ -4,7 +4,6 @@ const DigitrafficService = {
     getLatestCoordinate: function(value) {
         var url = "https://rata.digitraffic.fi/api/v1/train-locations/latest/" + value;
         return axios.get(url).then(data => {
-            console.log(data.data);
             return data.data;
         }).catch(error => {
             console.error(error);
